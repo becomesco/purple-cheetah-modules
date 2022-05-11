e5ba230d5be4764549b1f854ca816ceaf4dd38f7
Bbanez <banezprod@gmail.com>
2022-05-11 14:20:00 +0200

    Improve output - file per collection

--- fsdb/src/main.ts
+++ fsdb/src/main.ts
@@ -11,10 +11,12 @@ import type {
 import { useFS, useLogger } from '@becomes/purple-cheetah';
 import type { Module } from '@becomes/purple-cheetah/types';
 
-let output = path.join(process.cwd(), '.fsdb.json');
+let output = path.join(process.cwd(), '.fsdb');
 let saveInterval: NodeJS.Timeout;
 let cache: FSDBCache = {};
-let cacheHash = '';
+const cacheHash: {
+  [collection: string]: string;
+} = {};
 const logger = useLogger({
   name: 'FSDB',
 });
@@ -71,27 +73,45 @@ const fsdb: FSDB = {
   },
 };
 
-async function save(): Promise<void> {
-  const cacheString = JSON.stringify(cache);
-  const hash = crypto.createHash('sha256').update(cacheString).digest('hex');
-  if (hash !== cacheHash) {
-    await fs.save(output, cacheString);
-    cacheHash = hash;
-  }
-}
-async function init(config: FSDBConfig): Promise<void> {
-  if (await fs.exist(output, true)) {
-    const temp = (await fs.read(output)).toString();
-    if (!temp.startsWith('{}')) {
-      cache = JSON.parse(temp);
+async function save(col?: string): Promise<void> {
+  if (col && cache[col]) {
+    const collection = col;
+    const cacheString = JSON.stringify(cache[collection]);
+    const hash = crypto.createHash('sha256').update(cacheString).digest('hex');
+    if (hash !== cacheHash[collection]) {
+      await fs.save([output, collection + '.json'], cacheString);
+      cacheHash[collection] = hash;
     }
   } else {
-    await fs.save(output, '{}');
+    for (const collection in cache) {
+      const cacheString = JSON.stringify(cache[collection]);
+      const hash = crypto
+        .createHash('sha256')
+        .update(cacheString)
+        .digest('hex');
+      if (hash !== cacheHash[collection]) {
+        await fs.save([output, collection + '.json'], cacheString);
+        cacheHash[collection] = hash;
+      }
+    }
   }
-  cacheHash = crypto
-    .createHash('sha256')
-    .update(JSON.stringify(cache))
-    .digest('hex');
+}
+
+async function init(config: FSDBConfig): Promise<void> {
+  for (const collection in cache) {
+    if (await fs.exist([output, collection + '.json'], true)) {
+      cache[collection] = JSON.parse(
+        await fs.readString([output, collection + '.json']),
+      );
+    } else {
+      cache[collection] = {};
+      await fs.save([output, collection + '.json'], '{}');
+    }
+    cacheHash[collection] = crypto
+      .createHash('sha256')
+      .update(JSON.stringify(cache[collection]))
+      .digest('hex');
+  }
   saveInterval = setInterval(async () => {
     try {
       await save();
@@ -116,9 +136,9 @@ export function useFSDB(): FSDB {
 export function createFSDB(config: FSDBConfig): Module {
   if (config.output) {
     if (config.output.startsWith('/')) {
-      output = config.output + '.fsdb.json';
+      output = config.output;
     } else {
-      output = path.join(process.cwd(), config.output + '.fsdb.json');
+      output = path.join(process.cwd(), config.output);
     }
   }
   if (!config.saveInterval) {
